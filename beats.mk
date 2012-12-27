bb616d6695baf3a7a4b892d8bf4a470810eedd4b
justlove joyous <justlovejoy@gmail.com>
2012-12-27 01:48:40 -0500

    Different Beats Same Formula~jlj

--- beats.mk
+++ beats.mk
@@ -17,43 +17,131 @@
 ########################################################################
 
 PRODUCT_COPY_FILES += \
-	vendor/htc/beats/bin/basimage_ibeats_solo.bin:system/bin/basimage_ibeats_solo.bin \
-	vendor/htc/beats/bin/dynimage_ibeats_x.bin:system/bin/dynimage_ibeats_x.bin \
-	vendor/htc/beats/bin/dynimage_ibeats.bin:system/bin/dynimage_ibeats.bin \
-	vendor/htc/beats/bin/basimage_ibeats.bin:system/bin/basimage_ibeats.bin \
-	vendor/htc/beats/bin/peqimage_ibeats_solo_x.bin:system/bin/peqimage_ibeats_solo_x.bin \
-	vendor/htc/beats/bin/basimage_ibeats_solo_x.bin:system/bin/basimage_ibeats_solo_x.bin \
-	vendor/htc/beats/bin/peqimage_ibeats.bin:system/bin/peqimage_ibeats.bin \
-	vendor/htc/beats/bin/peqimage_ibeats_x.bin:system/bin/peqimage_ibeats_x.bin \
-	vendor/htc/beats/bin/dynimage_ibeats_solo_x.bin:system/bin/dynimage_ibeats_solo_x.bin \
-	vendor/htc/beats/bin/dynimage_ibeats_solo.bin:system/bin/dynimage_ibeats_solo.bin \
-	vendor/htc/beats/bin/basimage_ibeats_x.bin:system/bin/basimage_ibeats_x.bin \
-	vendor/htc/beats/bin/peqimage_ibeats_solo.bin:system/bin/peqimage_ibeats_solo.bin \
-	vendor/htc/beats/lib/soundfx:system/lib/soundfx \
-	vendor/htc/beats/lib/soundfx/libcyanogen-dsp.so:system/lib/soundfx/libcyanogen-dsp.so \
-	vendor/htc/beats/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so \
-	vendor/htc/beats/lib/soundfx/libbundlewrapper.so:system/lib/soundfx/libbundlewrapper.so \
-	vendor/htc/beats/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
-	vendor/htc/beats/lib/soundfx/libreverbwrapper.so:system/lib/soundfx/libreverbwrapper.so \
-	vendor/htc/beats/etc/audio_effects.conf:system/etc/audio_effects.conf \
-	vendor/htc/beats/etc/basimage_ibeats_solo.bin:system/etc/basimage_ibeats_solo.bin \
-	vendor/htc/beats/etc/dynimage_ibeats_x.bin:system/etc/dynimage_ibeats_x.bin \
-	vendor/htc/beats/etc/dynimage_ibeats.bin:system/etc/dynimage_ibeats.bin \
-	vendor/htc/beats/etc/basimage_ibeats.bin:system/etc/basimage_ibeats.bin \
-	vendor/htc/beats/etc/peqimage_gec.bin:system/etc/peqimage_gec.bin \
-	vendor/htc/beats/etc/peqimage_ibeats_solo_x.bin:system/etc/peqimage_ibeats_solo_x.bin \
-	vendor/htc/beats/etc/basimage_ibeats_solo_x.bin:system/etc/basimage_ibeats_solo_x.bin \
-	vendor/htc/beats/etc/dynimage_gec_x.bin:system/etc/dynimage_gec_x.bin \
-	vendor/htc/beats/etc/peqimage_ibeats.bin:system/etc/peqimage_ibeats.bin \
-	vendor/htc/beats/etc/basimage_gec_x.bin:system/etc/basimage_gec_x.bin \
-	vendor/htc/beats/etc/HP_Audio.csv:system/etc/HP_Audio.csv \
-	vendor/htc/beats/etc/basimage_gec.bin:system/etc/basimage_gec.bin \
-	vendor/htc/beats/etc/peqimage_ibeats_x.bin:system/etc/peqimage_ibeats_x.bin \
-	vendor/htc/beats/etc/dynimage_gec.bin:system/etc/dynimage_gec.bin \
-	vendor/htc/beats/etc/dynimage_ibeats_solo_x.bin:system/etc/dynimage_ibeats_solo_x.bin \
-	vendor/htc/beats/etc/dynimage_ibeats_solo.bin:system/etc/dynimage_ibeats_solo.bin \
-	vendor/htc/beats/etc/peqimage_gec_x.bin:system/etc/peqimage_gec_x.bin \
-	vendor/htc/beats/etc/basimage_ibeats_x.bin:system/etc/basimage_ibeats_x.bin \
-	vendor/htc/beats/etc/peqimage_ibeats_solo.bin:system/etc/peqimage_ibeats_solo.bin \
-	vendor/htc/beats/app/MusicFX.apk:system/app/MusicFX.apk \
-	vendor/htc/beats/app/BEATS.apk:system/app/BEATS.apk 
+vendor/htc/beats/bin/sound:system/bin/sound \
+vendor/htc/beats/bin/sound8x60:system/bin/sound8x60 \
+vendor/htc/beats/bin/spkamp:system/bin/spkamp \
+vendor/htc/beats/bin/basimage_gec.bin:system/bin/basimage_gec.bin \
+vendor/htc/beats/bin/basimage_gec_x.bin:system/bin/basimage_gec_x.bin \
+vendor/htc/beats/bin/basimage_ibeats.bin:system/bin/basimage_ibeats.bin \
+vendor/htc/beats/bin/basimage_ibeats_solo.bin:system/bin/basimage_ibeats_solo.bin \
+vendor/htc/beats/bin/basimage_ibeats_solo_x.bin:system/bin/basimage_ibeats_solo_x.bin \
+vendor/htc/beats/bin/basimage_ibeats_x.bin:system/bin/basimage_ibeats_x.bin \
+vendor/htc/beats/bin/dynimage_gec.bin:system/bin/dynimage_gec.bin \
+vendor/htc/beats/bin/dynimage_gec_x.bin:system/bin/dynimage_gec_x.bin \
+vendor/htc/beats/bin/dynimage_ibeats.bin:system/bin/dynimage_ibeats.bin \
+vendor/htc/beats/bin/dynimage_ibeats_solo.bin:system/bin/dynimage_ibeats_solo.bin \
+vendor/htc/beats/bin/dynimage_ibeats_solo_x.bin:system/bin/dynimage_ibeats_solo_x.bin \
+vendor/htc/beats/bin/dynimage_ibeats_x.binn:system/bin/dynimage_ibeats_x.bin \
+vendor/htc/beats/bin/mm-audio-alsa-test:system/bin/mm-audio-alsa-test \
+vendor/htc/beats/bin/mm-venc-omx-test720p:system/bin/mm-venc-omx-test720p \
+vendor/htc/beats/bin/mm-video-encdrv-test:system/bin/mm-video-encdrv-test \
+vendor/htc/beats/bin/peqimage_gec.bin:system/bin/peqimage_gec.bin \
+vendor/htc/beats/bin/peqimage_gec_x.bin:system/bin/peqimage_gec_x.bin \
+vendor/htc/beats/bin/peqimage_ibeats.bin:system/bin/peqimage_ibeats.bin \
+vendor/htc/beats/bin/peqimage_ibeats_solo.bin:system/bin/peqimage_ibeats_solo.bin \
+vendor/htc/beats/bin/peqimage_ibeats_solo_x.bin:system/bin/peqimage_ibeats_solo_x.bin \
+vendor/htc/beats/bin/peqimage_ibeats_x.bin:system/bin/peqimage_ibeats_x.bin \
+vendor/htc/beats/bin/snd:system/bin/snd \
+vendor/htc/beats/bin/snd8k:system/bin/snd8k \
+vendor/htc/beats/etc/AdieHWCodec_BEATS_HW.csv:system/etc/AdieHWCodec_BEATS_HW.csv \
+vendor/htc/beats/etc/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+vendor/htc/beats/etc/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+vendor/htc/beats/etc/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+vendor/htc/beats/etc/AIC3254_REG_XD.csv:system/etc/AIC3254_REG_XD.csv \
+vendor/htc/beats/etc/audio_effects.conf:system/etc/audio_effects.conf \
+vendor/htc/beats/etc/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+vendor/htc/beats/etc/TPA2051_CFG_BEATS_HW.csv:system/etc/TPA2051_CFG_BEATS_HW.csv \
+vendor/htc/beats/etc/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+vendor/htc/beats/etc/audio/aeqcoe.txt:system/etc/audio/aeqcoe.txt \
+vendor/htc/beats/etc/audio/eqfilter.txt:system/etc/audio/eqfilter.txt \
+vendor/htc/beats/etc/audio/lmfilter.txt:system/etc/audio/lmfilter.txt \
+vendor/htc/beats/etc/audio/situation.txt:system/etc/audio/situation.txt \
+vendor/htc/beats/etc/audio/soundbooster.txt:system/etc/audio/soundbooster.txt \
+vendor/htc/beats/etc/audio/stream_earpiece.txt:system/etc/audio/stream_earpiece.txt \
+vendor/htc/beats/etc/audio/stream_headset.txt:system/etc/audio/stream_headset.txt \
+vendor/htc/beats/etc/audio/stream_speaker.txt:system/etc/audio/stream_speaker.txt \
+vendor/htc/beats/etc/audio_effects.conf:system/etc/audio_effects.conf \
+vendor/htc/beats/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+vendor/htc/beats/etc/soundimage/Sound_Bass_Booster.txt:system/etc/soundimage/Sound_Bass_Booster.txt \
+vendor/htc/beats/etc/soundimage/Sound_Blues.txt:system/etc/soundimage/Sound_Blues.txt \
+vendor/htc/beats/etc/soundimage/Sound_Classical.txt:system/etc/soundimage/Sound_Classical.txt \
+vendor/htc/beats/etc/soundimage/Sound_Country.txt:system/etc/soundimage/Sound_Country.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dolby_A_HP.txt:system/etc/soundimage/Sound_Dolby_A_HP.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dolby_A_SPK.txt:system/etc/soundimage/Sound_Dolby_A_SPK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dolby_V_HP.txt:system/etc/soundimage/Sound_Dolby_V_HP.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dolby_V_SPK.txt:system/etc/soundimage/Sound_Dolby_V_SPK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dualmic_EP.txt:system/etc/soundimage/Sound_Dualmic_EP.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dualmic_SPK.txt:system/etc/soundimage/Sound_Dualmic_SPK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Dualmic.txt:system/etc/soundimage/Sound_Dualmic.txt \ 
+vendor/htc/beats/etc/soundimage/Sound_Jazz.txt:system/etc/soundimage/Sound_Jazz.txt \
+vendor/htc/beats/etc/soundimage/Sound_Latin.txt:system/etc/soundimage/Sound_Latin.txt \
+vendor/htc/beats/etc/soundimage/Sound_New_Age.txt:system/etc/soundimage/Sound_New_Age.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original_BCLK.txt:system/etc/soundimage/Sound_Original_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original_Rec_MFG.txt:system/etc/soundimage/Sound_Original_Rec_MFG.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original_Recording_BCLK.txt:system/etc/soundimage/Sound_Original_Recording_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original_SPK_BCLK.txt:system/etc/soundimage/Sound_Original_SPK_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original_SPK_MFG.txt:system/etc/soundimage/Sound_Original_SPK_MFG.txt \
+vendor/htc/beats/etc/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_HP_BCLK.txt:system/etc/soundimage/Sound_Phone_Original_HP_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_HP_WB.txt:system/etc/soundimage/Sound_Phone_Original_HP_WB.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_REC_BCLK.txt:system/etc/soundimage/Sound_Phone_Original_REC_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_REC_NEL.txt:system/etc/soundimage/Sound_Phone_Original_REC_NEL.txt \ 
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_REC_WB.txt:system/etc/soundimage/Sound_Phone_Original_REC_WB.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_SPK_BCLK.txt:system/etc/soundimage/Sound_Phone_Original_SPK_BCLK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Phone_Original_SPK_WB.txt:system/etc/soundimage/Sound_Phone_Original_SPK_WB.txt \
+vendor/htc/beats/etc/soundimage/Sound_Piano.txt:system/etc/soundimage/Sound_Piano.txt \
+vendor/htc/beats/etc/soundimage/Sound_Pop.txt:system/etc/soundimage/Sound_Pop.txt \
+vendor/htc/beats/etc/soundimage/srs_bypass.cfg:system/etc/soundimage/srs_bypass.cfg \
+vendor/htc/beats/etc/soundimage/srsfx_trumedia_51.cfg:system/etc/soundimage/srsfx_trumedia_51.cfg \
+vendor/htc/beats/etc/soundimage/srsfx_trumedia_movie.cfg:system/etc/soundimage/srsfx_trumedia_movie.cfg \
+vendor/htc/beats/etc/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
+vendor/htc/beats/etc/soundimage/srsfx_trumedia_music_wide.cfg:system/etc/soundimage/srsfx_trumedia_music_wide.cfg \
+vendor/htc/beats/etc/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
+vendor/htc/beats/etc/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
+vendor/htc/beats/etc/soundimage/srs_global.cfg:system/etc/soundimage/srs_global.cfg \
+vendor/htc/beats/etc/soundimage/Sound_R_B.txt:system/etc/soundimage/Sound_R_B.txt \
+vendor/htc/beats/etc/soundimage/Sound_Recording_Mono.txt:system/etc/soundimage/Sound_Recording_Mono.txt \
+vendor/htc/beats/etc/soundimage/Sound_Rock.txt:system/etc/soundimage/Sound_Rock.txt \
+vendor/htc/beats/etc/soundimage/Sound_SRS_A_HP.txt:system/etc/soundimage/Sound_SRS_A_HP.txt \
+vendor/htc/beats/etc/soundimage/Sound_SRS_A_SPK.txt:system/etc/soundimage/Sound_SRS_A_SPK.txt \
+vendor/htc/beats/etc/soundimage/Sound_SRS_V_HP.txt:system/etc/soundimage/Sound_SRS_V_HP.txt \
+vendor/htc/beats/etc/soundimage/Sound_SRS_V_SPK.txt:system/etc/soundimage/Sound_SRS_V_SPK.txt \
+vendor/htc/beats/etc/soundimage/Sound_Treble_Booster.txt:system/etc/soundimage/Sound_Treble_Booster.txt \
+vendor/htc/beats/etc/soundimage/Sound_Vocal_Booster.txt:system/etc/soundimage/Sound_Vocal_Booster.txt \
+vendor/htc/beats/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+vendor/htc/beats/framework/com.srs.fusion.fx.jar:system/framework/com.srs.fusion.fx.jar \
+vendor/htc/beats/lib/libacdbmapper.so:system/lib/libacdbmapper.so \
+vendor/htc/beats/lib/libaudcal.so:system/lib/libaudcal.so \
+vendor/htc/beats/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
+vendor/htc/beats/lib/libaudioeffect_jni.so:system/lib/libaudioeffect_jni.so \
+vendor/htc/beats/lib/libaudioeq.so:system/lib/libaudioeq.so \
+vendor/htc/beats/lib/libaudio_init.so:system/lib/libaudio_init.so \
+vendor/htc/beats/lib/libAudioTrimmer.so:system/lib/libAudioTrimmer.so \
+vendor/htc/beats/lib/libbeatsbass.so:system/lib/libbeatsbass.so \
+vendor/htc/beats/lib/libbeatscorehtc.so:system/lib/libbeatscorehtc.so \
+vendor/htc/beats/lib/libbundlewrapper.so:system/lib/libbundlewrapper.so \
+vendor/htc/beats/lib/libreverbwrapper.so:system/lib/libreverbwrapper.so \
+vendor/htc/beats/lib/libsoundhoundaudio.so:system/lib/libsoundhoundaudio.so \
+vendor/htc/beats/lib/libSR_AudioIn.so:system/lib/libSR_AudioIn.so \
+vendor/htc/beats/lib/libsrsfx.so:system/lib/libsrsfx.so \
+vendor/htc/beats/lib/libvisualizer.so:system/lib/libvisualizer.so \
+vendor/htc/beats/lib/libvoAudioFR.so:system/lib/libvoAudioFR.so \
+vendor/htc/beats/lib/bluez-plugin/audio.so:system/lib/bluez-plugin/audio.so \
+vendor/htc/beats/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
+vendor/htc/beats/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
+vendor/htc/beats/lib/hw/hwcomposer.msm8660.so:system/lib/hw/hwcomposer.msm8660.so \
+vendor/htc/beats/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
+vendor/htc/beats/lib/soundfx/libbeatsbass.so:system/lib/soundfx/libbeatsbase.so \
+vendor/htc/beats/lib/soundfx/libbundlewrapper.so:system/lib/soundfx/libaudiopreprocessing.so \
+vendor/htc/beats/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libbundlewrapper.so \
+vendor/htc/beats/lib/soundfx/libcyanogen-dsp.so:system/lib/soundfx/libcyanogen-dsp.so \
+vendor/htc/beats/lib/soundfx/libreverbwrapper.so:system/lib/soundfx/libreverbwrapper.so \
+vendor/htc/beats/lib/soundfx/libsrsfx.so:system/lib/soundfx/libsrsfx.so \
+vendor/htc/beats/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so \
+vendor/htc/beats/lib/soundfx/libloudwrapper.so:system/lib/soundfx/libloudwrapper.so \
+vendor/htc/beats/app/HtcBeatsNotify.apk:system/app/HtcBeatsNotify.apk \
+vendor/htc/beats/app/HtcListen.apk:system/app/HtcListen.apk \
+vendor/htc/beats/app/HtcMusicEnhancer.apk:system/app/HtcMusicEnhancer.apk \
+vendor/htc/beats/app/HtcSounSetDownloadManager.apk:system/app/HtcSounSetDownloadManager.apk 
+
+
